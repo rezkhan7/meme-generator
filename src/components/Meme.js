dbe0b025546757c0a1c91b1acc3c4c0b8f4f2fda
Rezaur Khan <rezaurkhan@Rezaurs-MacBook-Air.local>
2022-01-16 18:38:11 -0500

    Utilized React's useState() and hooks to fetch image src data from memesData.js, which is acting as an API for now

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,15 +1,25 @@
 import React from "react"
+import  memesData from "../memesData.js"
 import * as style from "../style.css"
 
 let Meme = ()=>{
+    const [imgSrc, setImgSrc] = React.useState("")
+    
+    let getMemeImage = ()=>{
+        const memesArray = memesData.data.memes;
+        const randomNumber = Math.floor(Math.random()* memesArray.length)
+       setImgSrc(memesArray[randomNumber].url)
+        
+     
+    }
     return(
         <main>
-            <form className = "form">
+            <div className = "form">
                 <input type = "text" className = "form--input" placeholder="Top Text"></input>
                 <input type = "text" className = "form--input" placeholder="Bottom Text"></input>
-                <button className = "form--button">Get a new meme image 🖼</button>
-            </form>
-
+                <button className = "form--button" onClick = {getMemeImage}>Get a new meme image 🖼</button>
+            </div>
+            <img src = {imgSrc}></img>
         </main>
     )
 }
