72988cf614f1d284d39bf3c2a815132b4d45b77f
Rezaur Khan <rezaurkhan@Rezaurs-MacBook-Air.local>
2022-01-18 16:57:33 -0500

    Refactored state in order to account for the increasing complexity of this project

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,13 +3,26 @@ import  memesData from "../memesData.js"
 import * as style from "../style.css"
 
 let Meme = ()=>{
-    const [imgSrc, setImgSrc] = React.useState("")
+   // const [imgSrc, setImgSrc] = React.useState("http://i.imgflip.com/1bij.jpg")
+   const [meme, setMeme] = React.useState({
+       topText:"",
+       bottomText:"",
+       randomImage: "http://i.imgflip.com/1bij.jpg" 
+   })
+
+   const [allMemesData, setAllMemesData] = React.useState(memesData)
     
     let getMemeImage = ()=>{
         const memesArray = memesData.data.memes;
         const randomNumber = Math.floor(Math.random()* memesArray.length)
-       setImgSrc(memesArray[randomNumber].url)
-        
+        const url =  memesArray[randomNumber].url      
+        setMeme((prevState)=>{
+            return{
+                ...meme,
+                randomImage: url
+            }
+        })
+    
      
     }
     return(
@@ -19,7 +32,7 @@ let Meme = ()=>{
                 <input type = "text" className = "form--input" placeholder="Bottom Text"></input>
                 <button className = "form--button" onClick = {getMemeImage}>Get a new meme image 🖼</button>
             </div>
-            <img src = {imgSrc} className = "meme--image"></img>
+            <img src = {meme.randomImage} className = "meme--image"></img>
         </main>
     )
 }
